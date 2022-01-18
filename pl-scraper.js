7b646306ef93bb2f6984cbea3106ef79f2212e86
banochocolat <jonathan@youd.net>
2022-01-18 20:59:19 +0000

    latest

---
+++ pl-scraper.js
@@ -0,0 +1,36 @@
+//pl-scraper.js
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://www.premierleague.com/stats/top/players/saves';
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        console.log(html);
+        const $ = cheerio.load(html)
+        const goalsTable = $('.statsTableContainer > tr');
+        const topSavers = [];
+    
+
+    goalsTable.each(function () {
+        const rank = $(this).find('.rank').text();
+        const playerName = $(this).find('.playerName').text();
+        const nationality = $(this).find('.playerCountry').text();
+        const saves = $(this).find('.mainStat').text();
+    
+
+    topSavers.push({
+        rank,
+        name: playerName,
+        nationality, 
+        saves,
+    });
+    });
+
+    console.log(topSavers);
+
+    })
+    .catch(console.error);
+
