34b9b8b825942ba224c0438779aa525dd6318973
jonathan banerjee <jonathan@youd.net>
2022-04-03 17:28:09 +0100

    S3 bucket test

--- sum.js
+++ sum.js
@@ -2,15 +2,15 @@ function sum(a, b) {
   return a + b;
 }
 
-  function sum1(a, b) {
+function sum1(a, b) {
     return a - b;
   }
 
-  function sum2(a, b) {
+function sum2(a, b) {
     return a * b;
   }
 
-  function sum3(a, b) {
+function sum3(a, b) {
     return a / b;
   }
   module.exports = {sum, sum1, sum2, sum3}
